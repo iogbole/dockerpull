0bfcbd362d5933846c93efc7632b45e5b8042be7
Israel Ogbole <iogbole@gmail.com>
2021-02-02 11:23:08 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in {1..10}
+do
+   echo "Welcome $i times"
+    docker pull appdynamicscx/configmyapp
+    docker image rm appdynamicscx/configmyapp
+
+    docker pull appdynamicscx/mark-nodes-historical
+    docker image rm appdynamicscx/mark-nodes-historical
+done
+for
